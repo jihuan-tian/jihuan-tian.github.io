18e640363157b6fdc1119f7c307ca411840704c3
Jihuan Tian <jihuan_tian@hotmail.com>
2022-04-24 07:18:55 +0800

    2022-04-24-07-18

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 is_need_commit=`git status | grep "nothing to commit, working tree clean"` 
-echo $is_need_commit
+
 if [ -z "$is_need_commit" ]; then
-  echo "There is no change, now exit!"
-else
+  echo "Commit the changes!"
   git add --all
   git commit -m `date +%F-%H-%M`
+else
+  echo "There is no changes!"
 fi
 
-is_need_push=``
+git push
