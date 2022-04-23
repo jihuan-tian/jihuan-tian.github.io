e97e1c3412301ea106b7aac5150048895dab8e0e
Jihuan Tian <jihuan_tian@hotmail.com>
2022-04-24 07:13:23 +0800

    Add publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-git add --all
-git commit -m `date +%F-%H-%M`
-git push
+is_need_commit=`git status | grep "nothing to commit, working tree clean"` 
+echo $is_need_commit
+if [ -z "$is_need_commit" ]; then
+  echo "There is no change, now exit!"
+else
+  git add --all
+  git commit -m `date +%F-%H-%M`
+fi
+
+is_need_push=``
