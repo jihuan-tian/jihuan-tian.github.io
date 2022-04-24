da3bc7ac20bb5357ae9edd12bc7b682fe2da8520
Jihuan Tian <jihuan_tian@hotmail.com>
2022-04-24 08:52:09 +0800

    2022-04-24-08-52

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,11 +3,17 @@
 is_need_commit=`git status | grep "nothing to commit, working tree clean"` 
 
 if [ -z "$is_need_commit" ]; then
-  echo "Commit the changes!"
+  echo "=== Commit the changes!"
   git add --all
   git commit -m `date +%F-%H-%M`
 else
-  echo "There is no changes!"
+  echo "=== There are no changes to commit!"
 fi
 
-git push
+is_need_push=`git status | grep "Your branch is up to date with 'origin/main'."`
+if [ -z "$is_need_push" ]; then
+  echo "=== Start to push to remote branch!"
+  git push
+else
+  echo "There is nothing to push!"
+fi
