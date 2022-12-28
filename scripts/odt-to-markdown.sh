4517331a031aad031fc3110f9be419338bb1c109
Jihuan Tian <jihuan_tian@hotmail.com>
2022-12-29 06:20:06 +0800

    2022-12-29-06-20

--- scripts/odt-to-markdown.sh
+++ scripts/odt-to-markdown.sh
@@ -5,6 +5,6 @@ file_base_name=`basename "$1" .odt`
 # Convert odt to docx
 libreoffice --convert-to docx "$1"
 # Convert docx to html using Python package mammoth
-mammoth --output-format=html --output-dir=./ --style-map=../scripts/markdown-to-html-style-map "$file_base_name.docx"
+mammoth --output-format=html --output-dir=./ --style-map=../scripts/docx-to-html-style-map "$file_base_name.docx"
 # Convert html to markdown using pandoc
 pandoc -f html -t markdown -o "$file_base_name.markdown" "$file_base_name.html"
