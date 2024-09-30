f308a223bb06af83ac52e212df8d67fc217a1cd1
Jihuan Tian <jihuan_tian@hotmail.com>
2024-09-30 10:05:13 +0800

    2024-09-30-10-05

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+cd "$blog"
+
 is_need_commit=`git status | grep "nothing to commit, working tree clean"` 
 
 if [ -z "$is_need_commit" ]; then
   echo "=== Commit the changes!"
   git add --all
 
-  # Remove _config.yml from the commit.
+  # Remove _config.yml from the commit, since it is modified for local build.
   git restore --staged _config.yml
 
   if  [ "$#" -eq 1 ]; then
@@ -23,6 +25,30 @@ is_need_push=`git status | grep "Your branch is up to date with 'origin/main'."`
 if [ -z "$is_need_push" ]; then
   echo "=== Start to push to remote branch!"
   http_proxy=192.168.0.100:1080 https_proxy=192.168.0.100:1080 git push
+
+  # Go to the public folder and pull the latest modifications.
+  cd "$blog-public"
+  http_proxy=192.168.0.100:1080 https_proxy=192.168.0.100:1080 git pull
+  
+  # Build for public.
+  bundle exec jekyll build
+
+  cd _site
+  echo "=== Commit the changes for public!"
+  git add --all
+
+  if  [ "$#" -eq 1 ]; then
+      # When there is a command line argument, use it as the committing message.
+      git commit -m `date +%F-%H-%M`": $1"
+  else
+      git commit -m `date +%F-%H-%M`
+  fi
+
+  echo "=== Start to push to remote branch for public!"
+  http_proxy=192.168.0.100:1080 https_proxy=192.168.0.100:1080 git push
+
+  # Go back to blog directory.
+  cd "$blog"
 else
   echo "There is nothing to push!"
 fi
