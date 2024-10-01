6f6f79636849a97655da244ed36d97ed423267f8
Jihuan Tian <jihuan_tian@hotmail.com>
2024-10-01 16:12:07 +0800

    2024-10-01-16-12

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -23,7 +23,7 @@ fi
 
 is_need_push=`git status | grep "Your branch is up to date with 'origin/main'."`
 if [ -z "$is_need_push" ]; then
-  echo "=== Start to push to remote branch!"
+  echo "=== Start to push source to remote branch!"
   http_proxy=192.168.0.100:1080 https_proxy=192.168.0.100:1080 git push
 
   # Go to the public folder and pull the latest modifications.
@@ -31,10 +31,13 @@ if [ -z "$is_need_push" ]; then
   http_proxy=192.168.0.100:1080 https_proxy=192.168.0.100:1080 git pull
   
   # Build for public.
-  bundle exec jekyll build
+  bundle exec jekyll build --destination "$blog-public-build"
 
-  cd _site
-  echo "=== Commit the changes for public!"
+  # Synchronize the new build to the local branch for submit.
+  rsync -rtlvz --update "$blog-public-build/" "$blog-public-build-for-submit/"
+
+  cd "$blog-public-build-for-submit"
+  echo "=== Commit the changes of generated pages for public!"
   git add --all
 
   if  [ "$#" -eq 1 ]; then
@@ -44,7 +47,7 @@ if [ -z "$is_need_push" ]; then
       git commit -m `date +%F-%H-%M`
   fi
 
-  echo "=== Start to push to remote branch for public!"
+  echo "=== Start to push generated pages to remote branch for public!"
   http_proxy=192.168.0.100:1080 https_proxy=192.168.0.100:1080 git push
 
   # Go back to blog directory.
